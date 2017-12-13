2a7fec2b337b867c64619b4b2f14f1cf50686985
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-12 21:32:42 -0700

    Fix api url and remove CL

--- src/components/helper/backEndApiPost/backEndApiPost.js
+++ src/components/helper/backEndApiPost/backEndApiPost.js
@@ -1,16 +1,12 @@
 export const backEndApiPost = async (user) => {
     try {
-        const fetchBackEndApi = await fetch('http://localhost:3000/api/users', {
+        const fetchBackEndApi = await fetch('http://localhost:3000/api/users/new', {
             method: 'POST',
             body: JSON.stringify(user),
             headers: {
                 'Content-Type': 'application/json'
             }
         })
-
-        const response = await fetchBackEndApi.json()
-        console.log(response)
-        return response;
 
     } catch (e) {
         return new Error('Failed to reach backend database')
