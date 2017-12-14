d4705ec1273f9c09159613885dcc4feb76f61623
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-13 22:18:02 -0700

    Remove mapDispatchToProps

--- src/containers/CardContainer/CardContainer.js
+++ src/containers/CardContainer/CardContainer.js
@@ -4,38 +4,30 @@ import Card from '../../components/Card/Card';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
-export const CardContainer = ( props ) => {
-	const cards = props.movies.map((card) => {
-		return (
-			<Card
-				key={card.movieId}
-				userId={card.userId}
-				id={card.movieId}
-				title={card.title}
-				image={card.image}
-				releaseDate={card.releaseDate}
-				voteAverage={card.voteAverage}
-				overview={card.overview}
-				favorite={card.favorite} />
-		)
-	})
+export const CardContainer = props => {
+  const cards = props.movies.map(card => {
+    return (
+      <Card
+        key={card.movieId}
+        userId={card.userId}
+        id={card.movieId}
+        title={card.title}
+        image={card.image}
+        releaseDate={card.releaseDate}
+        voteAverage={card.voteAverage}
+        overview={card.overview}
+        favorite={card.favorite}
+      />
+    );
+  });
 
-	return (
-		<div className="card-container">
-			{cards}
-		</div>
-	)
-}
+  return <div className="card-container">{cards}</div>;
+};
 
-const mapStateToProps = (store) =>{
-	return {
-		movies: store.movies
-	}
-}
+export const mapStateToProps = store => {
+  return {
+    movies: store.movies
+  };
+};
 
-const mapDispatchToProps = (dispatch) =>{
-	return {}
-}
-
-
-export default withRouter(connect(mapStateToProps, null)(CardContainer))
+export default withRouter(connect(mapStateToProps, null)(CardContainer));
