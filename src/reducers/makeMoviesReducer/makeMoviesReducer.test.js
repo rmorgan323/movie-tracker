1c870aec4bc2436ae43f5bc6107dec334ec714d8
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-13 21:03:55 -0700

    Create data directory and mockMovie.js for testing

--- src/reducers/makeMoviesReducer/makeMoviesReducer.test.js
+++ src/reducers/makeMoviesReducer/makeMoviesReducer.test.js
@@ -2,9 +2,13 @@ import makeMoviesReducer from './makeMoviesReducer';
 import * as actions from '../../actions';
 
 describe('makeMoviesReducer tests', () => {
-    it('Should return the default store', () =>{
-        const expected = [];
+  it('Should return the default store', () => {
+    const expected = [];
 
-        expect(makeMoviesReducer(undefined, {})).toEqual(expected);
-    })
+    expect(makeMoviesReducer(undefined, {})).toEqual(expected);
+  });
+
+  it('Should return a new store with a movie', () => {
+      
+  })
 });
