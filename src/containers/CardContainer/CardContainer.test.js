06ae746ca74c1996cfcf720cb514b2d33fa334ba
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-13 22:18:22 -0700

    Write out remaining CardContainer tests and mapStateToProps tests

--- src/containers/CardContainer/CardContainer.test.js
+++ src/containers/CardContainer/CardContainer.test.js
@@ -1,21 +1,45 @@
-import { CardContainer } from './CardContainer';
+import { CardContainer, mapStateToProps } from './CardContainer';
 import React from 'react';
 import { shallow } from 'enzyme';
 import mockMovie from '../../data/mockMovie';
 
 describe('CardContainer tests', () => {
-    it('Should exist', () => {
-        const mockProps = { movies: [mockMovie] }
-        console.log(mockProps)
-        const renderedCardContainer = shallow(<CardContainer props={mockProps}/>);
-        console.log(renderedCardContainer.debug())
-    })
+  it('Should exist', () => {
+    const mockProps = {
+      movies: []
+    };
+    console.log(mockProps);
 
-    it('Should match the snapshot', () => {
+    const renderedCardContainer = shallow(<CardContainer props={mockProps} />);
+    console.log(renderedCardContainer.debug());
 
-    })
+    expect(renderedCardContainer).toBeDefined();
+  });
 
-    it('Should display cards from props', () => {
+  it('Should match the snapshot', () => {
+    const renderedCardContainer = shallow(<CardContainer />);
 
-    })
-})
+    expect(renderedCardContainer).toMatchSnapshot();
+  });
+
+  it('Should display cards from props', () => {
+    const mockProps = {
+      movies: [mockMovie]
+    };
+
+    const renderedCardContainer = shallow(<CardContainer props={mockProps} />);
+
+    expect(renderedCardContainer.find('Card').length).toEqual(1);
+  });
+
+  describe('mapStateToProps tests', () => {
+    it('Should pull movies from store', () => {
+      const mockStore = {
+        movies: []
+      };
+      const result = mapStateToProps(mockStore);
+
+      expect(result.movies).toEqual(mockStore.movies);
+    });
+  });
+});
