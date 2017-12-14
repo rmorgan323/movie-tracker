aa763a5c57d87d4688248f7b7f1a852ebdd696ef
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-13 21:58:30 -0700

    Begin CardContainer tests

--- src/containers/CardContainer/CardContainer.test.js
+++ src/containers/CardContainer/CardContainer.test.js
@@ -1,11 +1,14 @@
 import { CardContainer } from './CardContainer';
 import React from 'react';
 import { shallow } from 'enzyme';
+import mockMovie from '../../data/mockMovie';
 
 describe('CardContainer tests', () => {
     it('Should exist', () => {
-        const mockProps = { movies: [{}] }
+        const mockProps = { movies: [mockMovie] }
+        console.log(mockProps)
         const renderedCardContainer = shallow(<CardContainer props={mockProps}/>);
+        console.log(renderedCardContainer.debug())
     })
 
     it('Should match the snapshot', () => {
