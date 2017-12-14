b6387f49ac85ef742602761c2e203d9b5237f3fd
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-13 21:20:44 -0700

    Mock expected CardContainer tests

---
+++ src/containers/CardContainer/CardContainer.test.js
@@ -0,0 +1,16 @@
+import CardContainer from './CardContainer';
+import { shallow } from 'enzyme';
+
+describe('CardContainer tests', () => {
+    it('Should exist', () => {
+
+    })
+
+    it('Should match the snapshot', () => {
+
+    })
+
+    it('Should display cards from props', () => {
+
+    })
+})
