f913b810bccd7ce481ccadc6f9a25f165314bc6b
mariastlouis <msantray@yahoo.com>
2017-12-15 12:07:07 -0700

    card changed to a function from a class

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,13 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './Card.css';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/'
 
- // {image, title, releaseDate, voteAverage, overview})
 
-class Card extends Component {
-	constructor () {
-    super()
-  }
 
-  componentWillUpdate (nextProps, nextState) {
-    console.log('nextprops', nextProps, 'nextstate', nextState)
-  
-  }
+const Card = ( {image, userId, title, releaseDate, voteAverage, overview}) => {
+	
 
-  render() {
-    // console.log('prps', this.props)
-   const {image, title, releaseDate, voteAverage, overview, userId} = this.props;
   return (
 
 		<div className="card">
@@ -26,10 +16,9 @@ class Card extends Component {
 			<h4>Released: {releaseDate}</h4>
 			<h4>Average Review: {voteAverage}</h4>
 			<h4 className="overview">Overview: {overview}</h4>
-      <button onClick = {()=> console.log('user', userId)}> Favorite </button>
+      <button onClick = {()=> console.log('hi')}> Favorite </button>
 		</div>
 	)
-}
 }
 
 export const mapStateToProps = store =>{
