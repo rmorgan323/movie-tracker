d0e44d2a0260cceafe78af1d2172c45e4107ec50
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-15 16:08:15 -0700

    Move componentDidMount to Header from App, as well as movie array population

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,35 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { userLogout } from '../../actions';
+import { getMovies } from '../../actions';
 
-const Header = (props) => {
-	let headerLinks;
+class Header extends Component {
+	componentDidMount = () => {
+    this.props.storeMovies()
+  }
 
-	if (Object.keys(props.user).length === 0) {
-		headerLinks = (
+	noUserRender = () => {
+		return (
 			<div>
 	      <NavLink className="login" to='/login'>Login</NavLink>
 	      <NavLink className="signup" to='/signup'>Sign-up</NavLink>
 			</div>
 		)
-	} else {
-		headerLinks = (
+	}
+
+	userRender = () => {
+		return (
 			<div>
-		    <div className="login">Welcome, {props.user.userInfo.name}</div>
-		    <NavLink className="signup" to='/' onClick={props.logoutUser} >Sign-Out</NavLink>
-	    </div>
+					<div className="login">Welcome, {this.props.user.userInfo.name}</div>
+					<NavLink className="signup" to='/' onClick={this.props.logoutUser} >Sign-Out</NavLink>
+				</div>
 		)
 	}
 
-	return (
-		<div className="header">
-		  <h1>Movie Tracker</h1>
-		  {headerLinks}
-		</div>
-	)
+	render() {
+		var headerLinks = determainUser(this.props.user)  ? this.noUserRender() 
+																											: this.userRender()
+		return (
+			<div className="header">
+				<h1>Movie Tracker</h1>
+				{headerLinks}
+			</div>
+		)
+	}
 }
 
 export const mapStateToProps = store => {
@@ -40,9 +49,9 @@ export const mapStateToProps = store => {
 
 export const mapDispatchToProps = (dispatch) => {
 	return {
-		logoutUser: () => dispatch(userLogout()) 
+		logoutUser: () => dispatch(userLogout()),
+		storeMovies: () => dispatch(getMovies())
 	}
 }
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Header));
-// export default Header;
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
