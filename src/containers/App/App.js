1025f40f49877b5575fc4be948cfe4d2544883f9
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-16 09:27:13 -0700

    Import and render Routes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,23 +1,17 @@
 import React, { Component } from 'react';
-import { Route, withRouter } from 'react-router-dom';
 import Header from '../../components/Header/Header';
-import ControlledForm from '../ControlledForm/ControlledForm';
-import CardContainer from '../CardContainer/CardContainer';
+import Routes from '../Routes/Routes';
 import './App.css';
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
         <Header />
-
-        <Route path='/login' component={ControlledForm} />
-        <Route path='/signup' component={ControlledForm}/>
-        <Route exact path='/' component={CardContainer} />
+        <Routes />
       </div>
     );
   }
 }
 
-export default App
+export default App;
