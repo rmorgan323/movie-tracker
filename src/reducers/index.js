401971a82479a033e6b2cbeb7cf9b4073ccc5c9f
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 10:29:18 -0700

    Fix filepath

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 import makeMoviesReducer from './makeMoviesReducer/makeMoviesReducer';
-import userReducers from './userReducers';
+import userReducers from './userReducers/userReducers';
 
 const rootReducer = combineReducers({
   movies: makeMoviesReducer,
