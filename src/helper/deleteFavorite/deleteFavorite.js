380e3f38fa663f0170fcbef2c15e87e67bc850c2
mariastlouis <msantray@yahoo.com>
2017-12-17 07:27:52 -0700

    user sign in and determine user linted

--- src/helper/deleteFavorite/deleteFavorite.js
+++ src/helper/deleteFavorite/deleteFavorite.js
@@ -2,7 +2,7 @@ export const removeFavorite = async (userId, movieId) => {
   const remove = await fetch(`http://localhost:3000/api/users/${userId}/favorites/${movieId}`, {
     method: 'DELETE',
     headers: {
-    'Content-Type': 'application/json'
+      'Content-Type': 'application/json'
     }
   });
 };
