66f799b97c50d64b051aca51037096db60367d0c
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 11:48:19 -0700

    Add beforeEach to CardContainer tests

--- src/containers/CardContainer/CardContainer.test.js
+++ src/containers/CardContainer/CardContainer.test.js
@@ -4,16 +4,30 @@ import { shallow } from 'enzyme';
 import mockMovie from '../../data/mockMovie';
 
 describe('CardContainer tests', () => {
-  it('Should match the snapshot', () => {
-    const renderedCardContainer = shallow(<CardContainer movies={[]} />);
+  let mockMovies;
+  let mockUser;
+  let mockLocation;
+  let renderedCardContainer;
 
+  beforeEach(() => {
+    mockMovies = [];
+    mockUser = {};
+    mockLocation = {};
+
+    renderedCardContainer = shallow(
+      <CardContainer
+        movies={mockMovies}
+        user={mockUser}
+        location={mockLocation}
+      />
+    );
+  });
+  it('Should match the snapshot', () => {
     expect(renderedCardContainer).toMatchSnapshot();
   });
 
   it('Should display cards from props', () => {
-    const mockMovies = [mockMovie];
-
-    const renderedCardContainer = shallow(<CardContainer movies={mockMovies} />);
+    mockMovies = [mockMovie];
 
     expect(renderedCardContainer.find('Card').length).toEqual(1);
   });
