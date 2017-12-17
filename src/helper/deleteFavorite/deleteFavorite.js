1e1b48cd1a5c1ce66fea30882fc2c764a426cd92
mariastlouis <msantray@yahoo.com>
2017-12-17 07:25:16 -0700

    delete favorites linted

--- src/helper/deleteFavorite/deleteFavorite.js
+++ src/helper/deleteFavorite/deleteFavorite.js
@@ -1,10 +1,10 @@
 export const removeFavorite = async (userId, movieId) => {
-	const remove = await fetch(`http://localhost:3000/api/users/${userId}/favorites/${movieId}`, {
-		method: 'DELETE',
-		headers: {
-			'Content-Type': 'application/json'
-		}
-	})
-}
+  const remove = await fetch(`http://localhost:3000/api/users/${userId}/favorites/${movieId}`, {
+    method: 'DELETE',
+    headers: {
+    'Content-Type': 'application/json'
+    }
+  });
+};
 
 export default removeFavorite;
