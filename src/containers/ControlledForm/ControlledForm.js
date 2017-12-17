1a9f879652058cd8575c3477731fcd5d9952256d
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 10:48:18 -0700

    Fix spacing and adjust error messages

--- src/containers/ControlledForm/ControlledForm.js
+++ src/containers/ControlledForm/ControlledForm.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import * as actions from '../../actions';
 import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import './ControlledForm.css';
 import PropTypes from 'prop-types';
+import * as actions from '../../actions';
+import './ControlledForm.css';
 
 class ControlledForm extends Component {
   constructor() {
@@ -18,31 +18,35 @@ class ControlledForm extends Component {
     };
   }
 
-  handleInputChange = (event) => {
+  handleInputChange = event => {
     let value = event.target.value;
     let property = event.target.className;
 
     this.setState({
-      users:{...this.state.users, [property]: value}
+      users: { ...this.state.users, [property]: value }
     });
-  }
+  };
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.user === 'error-signup') {
-      /*eslint-disable */
-      this.setState({errorMessage: 'User email is being used. Use another email or login with email'})
-      /*eslint-enable */
+      this.setState({
+        errorMessage:
+          'That email is already registered. Please use another email or try logging in.'
+      });
+
     } else if (nextProps.user === 'error-login') {
-      /*eslint-disable */
-      this.setState({errorMessage: 'Email and password do not match. Try again or create an account'})
-      /*eslint-enable */
+      this.setState({
+        errorMessage:
+          'That email and password do not match. Try again or create an account.'
+      });
+
     } else if (nextProps.user.signedIn === true) {
       this.props.history.push('/');
       this.props.getUserFavorites(nextProps.user.userInfo.id);
     }
   }
 
-  handleSubmit = async(event) => {
+  handleSubmit = async event => {
     event.preventDefault();
     const endPoint = this.props.location.pathname;
 
@@ -51,65 +55,80 @@ class ControlledForm extends Component {
     } else if (endPoint === '/signup') {
       await this.props.getUsers(this.state.users);
     }
-  }
+  };
 
   render() {
     let endPoint = this.props.location.pathname;
     return (
-      <div className = 'form-component'>
-        <div className = "form-button-container">
-          <div className = "login-buttons">
-            <NavLink className="login-button form-button" to='/login'>Login</NavLink>
-            <NavLink className="login-button form-button" to='/signup'>Sign-up</NavLink>
-          </div>   
-          <form onSubmit={this.handleSubmit}>
-            {endPoint === '/signup' && 
-          <input className='name' 
-            type='text' 
-            placeholder='Name' 
-            onChange={this.handleInputChange} 
-            value={this.state.users.name} />}
-
-            <input className='email' 
-              type='text' 
-              placeholder='Email' 
-              onChange={this.handleInputChange} 
-              value={this.state.users.email} />
-
-            <input className='password' 
-              type='password' 
-              placeholder='Password' 
-              onChange={this.handleInputChange} 
-              value={this.state.users.password}/>
-
-            <button type='submit'
-              className = "submit-button" 
-              onClick={this.handleSubmit} >Submit</button>
-          </form>
-          <div className = "error-message">
-            {this.state.errorMessage}
+      <div className="form-component">
+        <div className="form-button-container">
+          <div className="login-buttons">
+            <NavLink className="login-button form-button" to="/login">
+              Login
+            </NavLink>
+            <NavLink className="login-button form-button" to="/signup">
+              Sign-up
+            </NavLink>
           </div>
+
+          <form onSubmit={this.handleSubmit}>
+            {endPoint === '/signup' && (
+              <input
+                className="name"
+                type="text"
+                placeholder="Name"
+                onChange={this.handleInputChange}
+                value={this.state.users.name}
+              />
+            )}
+
+            <input
+              className="email"
+              type="text"
+              placeholder="Email"
+              onChange={this.handleInputChange}
+              value={this.state.users.email}
+            />
+
+            <input
+              className="password"
+              type="password"
+              placeholder="Password"
+              onChange={this.handleInputChange}
+              value={this.state.users.password}
+            />
+
+            <button
+              type="submit"
+              className="submit-button"
+              onClick={this.handleSubmit}
+            >
+              Submit
+            </button>
+          </form>
+
+          <div className="error-message">{this.state.errorMessage}</div>
         </div>
       </div>
     );
   }
 }
 
-const mapStateToProps = (store) => {
+const mapStateToProps = store => {
   return {
     user: store.user
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    getUsers: (user) => {
+    getUsers: user => {
       dispatch(actions.fetchUsers(user));
     },
-    checkUsers: (user) => {
+    checkUsers: user => {
       dispatch(actions.checkForUser(user));
     },
-    getUserFavorites: (id) => {
+    getUserFavorites: id => {
       dispatch(actions.checkUserFavorites(id));
     }
   };
