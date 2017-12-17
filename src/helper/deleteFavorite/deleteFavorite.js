8ba5aa523ef029affbb6b84b8ca8c0492e4c4601
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 07:44:05 -0700

    Return response to determine success of deleteFavorite

--- src/helper/deleteFavorite/deleteFavorite.js
+++ src/helper/deleteFavorite/deleteFavorite.js
@@ -1,10 +1,17 @@
 export const removeFavorite = async (userId, movieId) => {
-  const remove = await fetch(`http://localhost:3000/api/users/${userId}/favorites/${movieId}`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json'
+  const removeFetch = await fetch(
+    `http://localhost:3000/api/users/${userId}/favorites/${movieId}`,
+    {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
-  });
+  );
+
+  const response = await removeFetch.json();
+
+  return response;
 };
 
 export default removeFavorite;
