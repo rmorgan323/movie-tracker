b491b0b0cc47663de6bd3f804e620e2583b7f81b
mariastlouis <msantray@yahoo.com>
2017-12-17 07:18:10 -0700

    header linted

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,47 +1,48 @@
 import React, { Component } from 'react';
-import { NavLink, withRouter } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { userLogout } from '../../actions';
-import { getMovies } from '../../actions';
+import { userLogout, getMovies } from '../../actions';
 import determineUser from '../../helper/determineUser/determineUser';
 import './Header.css';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
-	componentDidMount = () => {
-    this.props.storeMovies()
+  componentDidMount = () => {
+    this.props.storeMovies();
   }
 
-	noUserRender = () => {
-		return (
-			<div className="header-box">
-	      <NavLink className="login nav-link" to='/login'>Login</NavLink>
-	      <NavLink className="signup nav-link" to='/signup'>Sign-up</NavLink>
-			</div>
-		)
-	}
+  noUserRender = () => {
+    return (
+      <div className="header-box">
+        <NavLink className="login nav-link" to='/login'>Login</NavLink>
+        <NavLink className="signup nav-link" to='/signup'>Sign-up</NavLink>
+      </div>
+    );
+  }
 
-	userRender = () => {
-		return (
-			<div className="header-box">
-				<div className="login login-message">Welcome, {this.props.user.userInfo.name}</div>
-				<NavLink to='/favorites'><button>FAVORITES</button></NavLink>
-				<NavLink className="signup nav-link" to='/' onClick={this.props.logoutUser} >Sign-Out</NavLink>
-			</div>
-		)
-	}
+userRender = () => {
+  return (
+    <div className="header-box">
+      <div className="login login-message">Welcome, {this.props.user.userInfo.name}</div>
+      <NavLink to='/favorites'><button>FAVORITES</button></NavLink>
+      <NavLink className="signup nav-link" to='/' 
+        onClick={this.props.logoutUser} >Sign-Out</NavLink>
+    </div>
+  );
+}
 
-	render() {
-		var headerLinks = determineUser(this.props.user)  ? this.userRender()
-																											: this.noUserRender()
-		return (
-			<div className="header">
-				<NavLink to='/' >
-					<h1><span>MOVIE</span>TRACKER</h1>
-				</NavLink>
-				{headerLinks}
-			</div>
-		)
-	}
+render() {
+  var headerLinks = determineUser(this.props.user)  ? this.userRender()
+    : this.noUserRender();
+  return (
+    <div className="header">
+      <NavLink to='/' >
+        <h1><span>MOVIE</span>TRACKER</h1>
+      </NavLink>
+      {headerLinks}
+    </div>
+  );
+}
 }
 
 export const mapStateToProps = store => {
@@ -51,10 +52,18 @@ export const mapStateToProps = store => {
 };
 
 export const mapDispatchToProps = (dispatch) => {
-	return {
-		logoutUser: () => dispatch(userLogout()),
-		storeMovies: () => dispatch(getMovies())
-	}
-}
+  return {
+    logoutUser: () => dispatch(userLogout()),
+    storeMovies: () => dispatch(getMovies())
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
+
+Header.propTypes = {
+// location: PropTypes.string,
+  storeMovies: PropTypes.array,
+  user: PropTypes.object,
+  logoutUser: PropTypes.func
+};
+
