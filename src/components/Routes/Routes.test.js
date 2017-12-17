6e3b7dee7a8dc0f92487ba734494d44f2e9af129
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 14:57:11 -0700

    Finish Routes testing suite

--- src/components/Routes/Routes.test.js
+++ src/components/Routes/Routes.test.js
@@ -3,5 +3,9 @@ import React from 'react';
 import { shallow } from 'enzyme';
 
 describe('Routes tests', () => {
+  it('Should match the snapshot', () => {
+    const renderedRoutes = shallow(<Routes />);
 
+    expect(renderedRoutes).toMatchSnapshot();
+  });
 });
