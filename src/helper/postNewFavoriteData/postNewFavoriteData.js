1c0ea6657eec9a639cb09c107f76f19a69c897ce
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 09:30:21 -0700

    Changed addFavorite helper to postNewFavoriteData for clarity

---
+++ src/helper/postNewFavoriteData/postNewFavoriteData.js
@@ -0,0 +1,34 @@
+const postNewFavoriteData = async ({
+  movieId,
+  image,
+  userId,
+  title,
+  releaseDate,
+  voteAverage,
+  overview
+}) => {
+  /*eslint-disable camelcase */
+  const fetchFavorite = await fetch(
+    'http://localhost:3000/api/users/favorites/new',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        movie_id: movieId,
+        user_id: userId,
+        title: title,
+        poster_path: image,
+        release_date: releaseDate,
+        vote_average: voteAverage,
+        overview: overview
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  );
+  let response = await fetchFavorite.json();
+
+  return response;
+};
+
+export default postNewFavoriteData;
