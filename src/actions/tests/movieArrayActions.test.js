e4bc55fc446b5c8f162a34e8ec9edd6d5ae1dce9
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 11:40:01 -0700

    Adjusted test title

--- src/actions/tests/movieArrayActions.test.js
+++ src/actions/tests/movieArrayActions.test.js
@@ -2,7 +2,7 @@ import * as actions from '../index';
 import mockMovie from '../../data/mockMovie';
 
 describe('Movie Array Actions tests', () => {
-  it('Has a type of MAKE_MOVIE_ARRAY', () => {
+  it('makeMovieArray has a type of MAKE_MOVIE_ARRAY', () => {
     const movies = [mockMovie];
     const expected = {
       type: 'MAKE_MOVIE_ARRAY',
