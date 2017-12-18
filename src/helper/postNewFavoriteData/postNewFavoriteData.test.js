fad7ef5ca49f1bb0bb25222fc91492df2623c795
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 21:21:28 -0700

    Complete postNewFavoriteData test suite

--- src/helper/postNewFavoriteData/postNewFavoriteData.test.js
+++ src/helper/postNewFavoriteData/postNewFavoriteData.test.js
@@ -1,6 +1,9 @@
 import postNewFavoriteData from './postNewFavoriteData.js';
 
-import { mockPostMovieData, mockPostMovieResponse } from '../../data/mockPostMovieData.js';
+import {
+  mockPostMovieData,
+  mockPostMovieResponse
+} from '../../data/mockPostMovieData.js';
 
 describe('add new user', () => {
   window.fetch = jest.fn().mockImplementation(() =>
@@ -13,35 +16,13 @@ describe('add new user', () => {
     expect(postNewFavoriteData).toBeAFunction;
   });
 
-  it.skip('should render the fetch data', async () => {
-    const fetch = await postNewFavoriteData();
+  it('should render the fetch data', async () => {
+    const fetch = await postNewFavoriteData(mockPostMovieData);
     expect(typeof fetch).toEqual('object');
   });
 
-  it.skip('should give a success response and userId upon submission', async () => {
-    const fetch = await postNewFavoriteData();
+  it('should give a success response and userId upon submission', async () => {
+    const fetch = await postNewFavoriteData(mockPostMovieData);
     expect(fetch).toEqual(mockPostMovieResponse);
-  });
-
-  it.skip('postNewFavoriteData is called with the correct params', async () => {
-    const expected = [
-      'http://localhost:3000/api/users/favorites/new',
-      {
-        body: {
-          movie_id: 317091,
-          user_id: 4,
-          title: 'November Criminals',
-          poster_path: "https://image.tmdb.org/t/p/w500/m2JxGgarPvtemudLZ1CB6gLJeUb.jpg",
-          release_date: "12/08/2017",
-          vote_average: "5.3",
-          overview: "A teenager takes on his own investigation of a murder in Washington D.C."
-        },
-        headers: { 'Content-Type': 'application/json' },
-        method: 'POST'
-      }
-    ];
-
-    await postNewFavoriteData(mockPostMovieData);
-    expect(window.fetch).toHaveBeenCalledWith(...expected);
   });
 });
