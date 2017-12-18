e872015da75d41385f7b1eddd4c0e9a251e52078
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 18:13:05 -0700

    Add beforeEach to ControlledForm tests

--- src/containers/ControlledForm/ControlledForm.test.js
+++ src/containers/ControlledForm/ControlledForm.test.js
@@ -4,7 +4,9 @@ import {
   mapDispatchToProps
 } from './ControlledForm';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
+import mockEmptyUser from '../../data/mockEmptyUser';
+import mockRealUser from '../../data/mockRealUser';
 
 const defaultUserState = {
   name: '',
@@ -15,38 +17,40 @@ const defaultUserState = {
 const defaultErrorState = '';
 
 describe('ControlledForm tests', () => {
-  it('should render component', () => {
-    const renderedControlledForm = shallow(
+  let renderedControlledForm;
+
+  beforeEach(() => {
+    renderedControlledForm = shallow(
       <ControlledForm user={{}} location={{}} />
-    );
+    )
+  })
+  it('Should render component', () => {
     expect(renderedControlledForm.find('.form-component').length).toEqual(1);
   });
 
-  it('should have a default user state and a default error message state', () => {
-    const renderedControlledForm = shallow(
-      <ControlledForm user={{}} location={{}} />
-    );
+  it('Should have a default user state and a default error message state', () => {
     expect(renderedControlledForm.state('users')).toEqual(defaultUserState);
     expect(renderedControlledForm.state('errorMessage')).toEqual(
       defaultErrorState
     );
   });
 
-  it.skip('should set state when a signup error is passed in from nextProps', () => {
-    const renderedControlledForm = shallow(
+  it('Should set state when a signup error is passed in from nextProps', () => {
+    renderedControlledForm = shallow(
       <ControlledForm user={'error-signup'} location={{}} />
     );
     const signupErrorMessage =
       'That email is already registered. Please use another email or try logging in.';
-    renderedControlledForm.update();
+
+    console.log(renderedControlledForm.state())
     expect(renderedControlledForm.state('errorMessage')).toEqual(
       signupErrorMessage
     );
   });
 
-  it.skip('should set state when a login error is passed in from nextProps', () => {});
+  it.skip('Should set state when a login error is passed in from nextProps', () => {});
 
-  it.skip('should set state for users when inputs contain text', () => {
+  it.skip('Should set state for users when inputs contain text', () => {
     const renderedControlledForm = shallow(
       <ControlledForm user={{}} location={{}} />
     );
