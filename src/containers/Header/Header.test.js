e841383d8d415d401554e5a832b8942b89403818
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 17:31:00 -0700

    Complete Header component tests

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -1,21 +1,47 @@
 import { Header, mapStateToProps, mapDispatchToProps } from './Header';
 import React from 'react';
 import { shallow } from 'enzyme';
-import mockUserData from '../../data/mockRealUser';
+import mockEmptyUser from '../../data/mockEmptyUser';
+import mockRealUser from '../../data/mockRealUser';
 
 describe('Header tests', () => {
   let renderedHeader;
-  let mockUser;
   let mockStoreMovies;
+  let mockLogout;
 
   beforeEach(() => {
-    mockUser = mockUserData;
     mockStoreMovies = jest.fn();
+    mockLogout = jest.fn();
 
-    renderedHeader = shallow(<Header user={mockUser} storeMovies={mockStoreMovies} />)
-  })
+    renderedHeader = shallow(
+      <Header
+        user={mockEmptyUser}
+        storeMovies={mockStoreMovies}
+        logoutUser={mockLogout}
+      />
+    );
+  });
 
   it('Should match the snapshot', () => {
-    expect(renderedHeader).toBeDefined();
-  })
+    expect(renderedHeader).toMatchSnapshot();
+  });
+
+  it('Should have login and signup button with no user', () => {
+    expect(renderedHeader.find('.login').length).toEqual(1);
+    expect(renderedHeader.find('.signup').length).toEqual(1);
+  });
+
+  it('Should have a favorites button with user logged in', () => {
+    expect(renderedHeader.find('button').length).toEqual(0);
+
+    renderedHeader = shallow(
+      <Header
+        user={mockRealUser}
+        storeMovies={mockStoreMovies}
+        logoutUser={mockLogout}
+      />
+    );
+
+    expect(renderedHeader.find('button').length).toEqual(1);
+  });
 });
