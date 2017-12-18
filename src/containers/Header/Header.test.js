2343dc956524c05daf5ad3931377981fa2c58fce
Hugh Hartigan <Hartigan.HM@gmail.com>
2017-12-17 17:10:42 -0700

    Pass initial Header test

--- src/containers/Header/Header.test.js
+++ src/containers/Header/Header.test.js
@@ -1,7 +1,21 @@
 import { Header, mapStateToProps, mapDispatchToProps } from './Header';
 import React from 'react';
 import { shallow } from 'enzyme';
+import mockUserData from '../../data/mockRealUser';
 
 describe('Header tests', () => {
+  let renderedHeader;
+  let mockUser;
+  let mockStoreMovies;
 
+  beforeEach(() => {
+    mockUser = mockUserData;
+    mockStoreMovies = jest.fn();
+
+    renderedHeader = shallow(<Header user={mockUser} storeMovies={mockStoreMovies} />)
+  })
+
+  it('Should match the snapshot', () => {
+    expect(renderedHeader).toBeDefined();
+  })
 });
